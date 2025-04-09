e777527d8bfa4c434e90bb337325034665f5b88b
unknown <darisahrahmani@gmail.com>
2025-04-09 14:11:37 +0700

    first commit

---
+++ src/assets/data/data.js
@@ -0,0 +1,122 @@
+export const data = {
+    bride: {
+        L: {
+            id: 1,
+            name: 'Abdurohman An-Nuriyah',
+            child: '(Subang)',
+            image: 'https://intancell.github.io/nikah/img/galeri/abdurohman.jpg'
+        },
+        P: {
+            id: 2,
+            name: 'Siti Nida urrohmah',
+            child: '(Majalengka)',
+            image: 'https://intancell.github.io/nikah/img/galeri/Nida.jpeg'
+        },
+
+        couple: './src/assets/images/couple.png'
+    },
+
+    time: {
+        marriage: {
+            year: '2025',
+            month: 'April',
+            date: '19',
+            day: 'Sabtu',
+            hours: {
+                start: '08.00',
+                finish: 'Selesai'
+            }
+        },
+        reception: {
+            year: '2025',
+            month: 'April',
+            date: '19',
+            day: 'Sabtu',
+            hours: {
+                start: '11.00',
+                finish: 'Selesai'
+            }
+        },
+        address: 'Kediaman mempelai wanita Blok Pasir Kenar RT Ojak Ds. Lebakwangi Kec. Malausna'
+    },
+
+    link: {
+        calendar: 'https://calendar.app.google/Y54U6TXTKpzCCsHy6',
+        map: 'https://maps.app.goo.gl/q1Ask2Jgd4ekiiKBA',
+    },
+
+    galeri: [
+        {
+            id: 1,
+            image: './src/assets/images/1.png'
+        },
+        {
+            id: 2,
+            image: './src/assets/images/2.png'
+        },
+        {
+            id: 3,
+            image: './src/assets/images/3.png'
+        },
+        {
+            id: 4,
+            image: './src/assets/images/4.png'
+        },
+        {
+            id: 5,
+            image: './src/assets/images/5.png'
+        }
+    ],
+
+    bank: [
+        {
+            id: 1,
+            name: 'Abdurohman An-Nuriyah',
+            icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Logo_dana_blue.svg/1200px-Logo_dana_blue.svg.png',
+            rekening: '081294463482'
+        },
+        {
+            id: 2,
+            name: 'Ipsum Lorem',
+            icon: './src/assets/images/bri.png',
+            rekening: '12345678'
+        },
+    ],
+
+    audio: './src/assets/audio/weding.mp3',
+
+    api: 'https://script.google.com/macros/s/AKfycbwQDTWj9voK8406Ox3KCBEkTKIMuOYCNRVSju-qmnuBUQAexlSvk9HQhlFuBad-XYY-/exec',
+
+    navbar: [
+        {
+            id: 1,
+            teks: 'Home',
+            icon: 'bx bxs-home-heart',
+            path: '#home',
+        },
+        {
+            id: 2,
+            teks: 'Mempelai',
+            icon: 'bx bxs-group',
+            path: '#bride',
+        },
+        {
+            id: 3,
+            teks: 'Tanggal',
+            icon: 'bx bxs-calendar-check',
+            path: '#time',
+        },
+        {
+            id: 4,
+            teks: 'Galeri',
+            icon: 'bx bxs-photo-album',
+            path: '#galeri',
+        },
+        {
+            id: 5,
+            teks: 'Ucapan',
+            icon: 'bx bxs-message-rounded-dots',
+            path: '#wishas',
+        },
+    ],
+}
